d0fc9684571e07f92e0c8ae508001009ad262b11
Kyle Mills <khonkhortisan@gmail.com>
2017-10-01 08:15:20 +0000

    disable type button on load

--- src/main/java/mod/render360/gui/advanced/FisheyeGui.java
+++ src/main/java/mod/render360/gui/advanced/FisheyeGui.java
@@ -15,11 +15,21 @@ public class FisheyeGui implements Advanced {
 
 	@Override
 	public void initGui(List<GuiButton> buttonList, int width, int height) {
-		buttonList.add(new GuiButton(18140, width / 2 - 190 + ((190*2-0*4)/5 + 0) * 0, height / 6 + 48, (190*2-0*4)/5, 20, "Stereographic"));
-		buttonList.add(new GuiButton(18141, width / 2 - 190 + ((190*2-0*4)/5 + 0) * 1, height / 6 + 48, (190*2-0*4)/5, 20, "Equidistant"));
-		buttonList.add(new GuiButton(18142, width / 2 - 190 + ((190*2-0*4)/5 + 0) * 2, height / 6 + 48, (190*2-0*4)/5, 20, "Equisolid"));
-		buttonList.add(new GuiButton(18143, width / 2 - 190 + ((190*2-0*4)/5 + 0) * 3, height / 6 + 48, (190*2-0*4)/5, 20, "Thoby"));
-		buttonList.add(new GuiButton(18144, width / 2 - 190 + ((190*2-0*4)/5 + 0) * 4, height / 6 + 48, (190*2-0*4)/5, 20, "Orthographic"));
+		GuiButton button0 = new GuiButton(18140, width / 2 - 190 + ((190*2-0*4)/5 + 0) * 0, height / 6 + 48, (190*2-0*4)/5, 20, "Stereographic");
+		GuiButton button1 = new GuiButton(18141, width / 2 - 190 + ((190*2-0*4)/5 + 0) * 1, height / 6 + 48, (190*2-0*4)/5, 20, "Equidistant"  );
+		GuiButton button2 = new GuiButton(18142, width / 2 - 190 + ((190*2-0*4)/5 + 0) * 2, height / 6 + 48, (190*2-0*4)/5, 20, "Equisolid"    );
+		GuiButton button3 = new GuiButton(18143, width / 2 - 190 + ((190*2-0*4)/5 + 0) * 3, height / 6 + 48, (190*2-0*4)/5, 20, "Thoby"        );
+		GuiButton button4 = new GuiButton(18144, width / 2 - 190 + ((190*2-0*4)/5 + 0) * 4, height / 6 + 48, (190*2-0*4)/5, 20, "Orthographic" );
+		button0.enabled = fisheye.fisheyeType!=0;
+		button1.enabled = fisheye.fisheyeType!=1;
+		button2.enabled = fisheye.fisheyeType!=2;
+		button3.enabled = fisheye.fisheyeType!=3;
+		button4.enabled = fisheye.fisheyeType!=4;
+		buttonList.add(button0);
+		buttonList.add(button1);
+		buttonList.add(button2);
+		buttonList.add(button3);
+		buttonList.add(button4);
 		
 		buttonList.add(new Slider(new Responder(), 18150, width / 2 - 155, height / 6 + 72, 150, 20, "Quality", 0.1f, 5f, fisheye.quality, 0.1f, null));
 		buttonList.add(new GuiButton(18151, width / 2 + 5, height / 6 + 72, 150, 20, "Antialiasing: " + (fisheye.antialiasing == 1 ? "OFF" : fisheye.antialiasing == 4 ? "LOW" : "HIGH")));
